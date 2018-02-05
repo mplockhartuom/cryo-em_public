927a31530b56d09645c35530e7be00257273f034
Michael Lockhart <michael.lockhart@manchester.ac.uk>
2018-02-05 09:19:29 +0000

    Move gauto code to public

---
+++ gauto/gauto-gauss.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+##############################
+#                            #
+#  Gauto-Gaussian script 0.1 #
+#                            #
+##############################
+
+# Please learn how to use Gautomatch before using this script
+# It is designed as a useful tool rather than an excuse not to
+#
+# Run this script from the same place you would run Relion (top directory)
+#
+# First a directory is made for running gautomatch
+mkdir gauto
+
+# Moving into the 'gauto' directory, symbolic links are made to micrographs
+cd gauto
+ln -sf ../Micrographs/*.mrc .
+
+# Variables
+echo What is the apix of the Micrographs?
+read apixM
+
+echo What is the particle diameter \(\Å\)?
+read diameter_var
+
+# Gautomatch 0.53 is run with a Gaussian blob.
+# All required commands should have been added.
+# Any additional commands can be added manually.
+/programs/x86_64-linux/gautomatch/0.53/bin/Gautomatch-v0.53_sm_20_cu7.5_x86_64 --apixM $apixM --diameter $diameter_var *.mrc
+
+# Here we wait for Gautomatch to finish
+wait
+
+# Moving to the 'Micrographs' directory we can make new symbolic links to the 
+# automatched particles in 'gauto'
+cd ../Micrographs
+ln -sf ../gauto/*automatch.star .
